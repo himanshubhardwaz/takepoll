bc44962b169d4251f3f99358157301160b2dccfe
Himanshu Bhardwaz <himanshu76200@gmail.com>
2022-11-19 09:43:58 +0530

    changed bg homepage

--- src/components/AnswerPoll.tsx
+++ src/components/AnswerPoll.tsx
@@ -11,17 +11,12 @@ function NoId() {
 
   return (
     <div className='hero min-h-screen'>
-      <Image
-        src='https://placeimg.com/1000/800/any/grayscale'
-        fill
-        alt={""}
-        className=''
-      />
+      <Image src='/home_bg.jpeg' fill alt={""} className='' />
       <div className='hero-overlay bg-opacity-60'></div>
       <div className='hero-content text-center text-neutral-content'>
         <div className='max-w-md'>
-          <h1 className='mb-5 text-5xl font-bold'>Hello there</h1>
-          <p className='mb-5'>
+          <h1 className='mb-5 text-5xl font-bold text-white'>Hello there</h1>
+          <p className='mb-5 text-white'>
             Create and conduct polls in a minute. Use it in your flipped
             classroom, in your lecture or just to amaze your audience. create
             your poll now!
