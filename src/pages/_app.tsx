a36dfee22bc58415b65f1c6b6e0074a2ea14c01d
Himanshu Bhardwaz <himanshu76200@gmail.com>
2022-11-19 17:02:10 +0530

    theme select

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,21 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import Layout from "@/components/Layout";
+import { useEffect } from "react";
+import { themeChange } from "theme-change";
 
 export const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    themeChange(false);
+  }, []);
   return (
     <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,16 @@ module.exports = {
     "./src/pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {},
+  daisyui: {
+    styled: true,
+    themes: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+    darkTheme: "dark",
+    themes: ["light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter"],
   },
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
 }
